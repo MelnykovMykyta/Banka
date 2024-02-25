d892ef852a50327193e5408cb4e42c4c61a527cf
MelnykovMykyta <melnikovnikita@ukr.net>
2024-02-26 01:38:31 +0200

    add MainTableView, MainTVC

---
+++ Banka/Helpers/Components/MainTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainTVC.swift
+//  Banka
+//
+//  Created by Nikita Melnikov on 26.02.2024.
+//
+
+import UIKit
+
+class MainTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Banka/Views/MainTableView.swift
@@ -0,0 +1,8 @@
+//
+//  MainTableView.swift
+//  Banka
+//
+//  Created by Nikita Melnikov on 26.02.2024.
+//
+
+import Foundation
