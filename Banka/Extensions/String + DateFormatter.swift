6484c56cc1f2925c3ccb3e552ee1cc6105989796
MelnykovMykyta <melnikovnikita@ukr.net>
2024-02-26 21:47:26 +0200

    add String + DateFormatter (Ext)

---
+++ Banka/Extensions/String + DateFormatter.swift
@@ -0,0 +1,32 @@
+//
+//  String + DateFormatter.swift
+//  Banka
+//
+//  Created by Nikita Melnikov on 26.02.2024.
+//
+
+import Foundation
+
+extension String {
+    
+    func getDayAndMonth() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        
+        guard let date = dateFormatter.date(from: self) else { return "" }
+        dateFormatter.dateFormat = "dd.MM"
+
+        return dateFormatter.string(from: date)
+    }
+    
+    func getYear() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        
+        guard let date = dateFormatter.date(from: self) else { return "" }
+        dateFormatter.dateFormat = "yyyy"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+}
